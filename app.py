368211d2af1568e0c599ddf7155553ab91e0347f
nihar <zanwarnihar@gmail.com>
2020-04-29 23:35:28 +0530

    multiple device support added. settings and configuration page added

--- app.py
+++ app.py
@@ -1,59 +1,179 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 import time
 import json
 import os
-from test import get_data, get_aotc
+from test import get_aotc, get_device_list, get_day_count, get_config_data, set_config_data, check_password
+from _thread import *
+from datetime import datetime
+
+global total_all_in, total_all_out
 
 app = Flask(__name__)
-initial_data = get_aotc('192.168.1.100')
-
-old_aotc = initial_data["occupancy"]
-old_in = initial_data["in"]
-old_out = initial_data["out"]
 UPLOAD_FOLDER = os.getcwd() + '/icons'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def get_old():
-    return old_aotc, old_in, old_out
+def print_my_data():
+    print(device_list)
+    print("total_all_occ", total_all_occ)
+    time.sleep(4)
 
 
-def set_old(old_aotc_here, old_in_here, old_out_here):
-    old_aotc = old_aotc_here
-    old_in = old_in_here
-    old_out = old_out_here
+def get_final_realtime_counts():
+    global total_all_occ
+    global device_list
+    global total_all_in, total_all_out
+    total_all_in = 0
+    total_all_out = 0
+    total_all_occ = 0
+
+    for i in range(0, len(device_list)):
+        if device_list[i]["active"]:
+            if device_list[i]["today_latest_in"] != device_list[i]["today_latest_in_old"]:
+                device_list[i]["aotc_in_old"] = device_list[i]["now_aotc_in"]
+                device_list[i]["today_latest_in_old"] = device_list[i]["today_latest_in"]
+                device_list[i]["final_in"] = device_list[i]["today_latest_in"]
+                print("five min  crossed")
+            else:
+                device_list[i]["final_in"] = device_list[i]["today_latest_in"] + (
+                            device_list[i]["now_aotc_in"] - device_list[i]["aotc_in_old"])
+
+            if device_list[i]["today_latest_out"] != device_list[i]["today_latest_out_old"]:
+                device_list[i]["aotc_out_old"] = device_list[i]["now_aotc_out"]
+                device_list[i]["today_latest_out_old"] = device_list[i]["today_latest_out"]
+                device_list[i]["final_out"] = device_list[i]["today_latest_out"]
+                print("five min  crossed")
+            else:
+                device_list[i]["final_out"] = device_list[i]["today_latest_out"] + (
+                            device_list[i]["now_aotc_out"] - device_list[i]["aotc_out_old"])
+            device_list[i]["final_occ"] = device_list[i]["final_in"] - device_list[i]["final_out"]
+            # print("Poll data", device_list)
+
+            total_all_in = total_all_in + device_list[i]["final_in"]
+            total_all_out = total_all_out + device_list[i]["final_out"]
+            total_all_occ = total_all_occ + device_list[i]["final_occ"]
+
+    # print("Poll data , Final Occ = ", total_all_occ, "total_all_in = " , total_all_in,  "total_all_out = " ,
+    # total_all_out )
+
+
+def maintain_device_list():
+    global device_list
+
+    for device in device_list:
+
+        result = get_aotc(device["ip"])
+        if result["success"] == 0:
+            device["active"] = False
+            continue
+
+    while 1:
+        for device in device_list:
+            if device["active"]:
+
+                response = get_aotc(device["ip"])
+                if response["success"] == 0:
+                    time.sleep(2)
+                    continue
+
+                device["now_aotc"] = response["occupancy"]
+                device["now_aotc_in"] = response["in"]
+                device["now_aotc_out"] = response["out"]
+                try:
+                    date = {
+                        "day": response["datetime"].split(' ')[0].split('/')[1],
+                        "year": response["datetime"].split(' ')[0].split('/')[2],
+                        "month": response["datetime"].split(' ')[0].split('/')[0]
+                    }
+                except:
+                    print("Error while parsing datetime" + response["datetime"])
+                    date = {
+                        "day": datetime.now().strftime("%d"),
+                        "year": datetime.now().strftime("%m"),
+                        "month": datetime.now().strftime("%Y")
+                    }
+
+                response2 = get_day_count(device["ip"], '5', date)
+
+                device["today_latest"] = response2["occupancy"]
+                device["today_latest_in"] = response2["in"]
+                device["today_latest_out"] = response2["out"]
+
+        get_final_realtime_counts()
+        time.sleep(2)
 
 
 @app.route('/home')
 def hello():
-
-
     return render_template('display.html')
+
+
+@app.route('/settings', methods=['GET'])
+def settings():
+    return render_template('settings.html')
+
+
+@app.route('/settings_values', methods=['GET', 'POST'])
+def get_settings_values():
+    if request.method == 'GET':
+        config = get_config_data()
+        if config == 0:
+            response = {
+                "offset": 0,
+                "success": 0,
+                "maximum-occupancy": 0
+            }
+            return json.dumps(response)
+        else:
+            response = {
+                "offset": config["relative_offset"],
+                "success": 1,
+                "maximum-occupancy": config["maximum_occupancy"]
+            }
+            return json.dumps(response)
+    elif request.method == 'POST':
+        config = get_config_data()
+        json_response = json.loads(request.get_data(as_text=True))
+        config["relative_offset"] = int(json_response["offset"])
+        config["maximum_occupancy"] = int(json_response["maximum_occupancy"])
+        if set_config_data(json.dumps(config)) == 0:
+            return json.dumps({"success": 0})
+        else:
+            return json.dumps({"success": 1})
 
 
 @app.route('/update_screen', methods=['GET'])
 def hello_name():
-    old_aotc, old_in, old_out = get_old()
-    result = get_data('192.168.1.100',old_aotc,old_in, old_out )
-    print(result)
-    with open("data.json", "r") as f:
-        response = json.load(f)
-    if result["occupancy"] >= response["maximum_occupancy"]:
+    global total_all_occ, total_all_out, total_all_in
 
-        response["occupancy_color"] = "darkred"
-        response["status"] = "STOP"
+    total_result = {
+        "success": 0,
+        "in": total_all_in,
+        "out": total_all_out,
+        "occupancy": 0,
+        "datetime": '',
+        "status": "",
+        "occupancy_color": "",
+        "maximum_occupancy": 0,
+        "config_error": False
+    }
+
+    config = get_config_data()
+    if config == 0:
+        total_result["config_error"] = True
+
+    total_result["occupancy"] = total_all_occ - config["relative_offset"]
+    total_result["maximum_occupancy"] = config["maximum_occupancy"]
+
+    if total_result["occupancy"] >= config["maximum_occupancy"]:
+        total_result["status"] = "STOP"
+        total_result["occupancy_color"] = "darkred"
     else:
-        response["occupancy_color"] = "darkgreen"
-        response["status"] = "GO"
-    #
-    # old_aotc = result["now_aotc"]
-    # old_in = result["now_in"]
-    # old_out = result["now_out"]
-    set_old(result["now_aotc"],result["now_in"], result["now_out"])
-    response["current_occupancy"] = result["occupancy"]
-    response["in_count"] = result["in"]
-    response["out_count"] = result["out"]
-    return json.dumps(response)
+        total_result["status"] = "GO"
+        total_result["occupancy_color"] = "darkgreen"
+
+    total_result["success"] = 1
+    return json.dumps(total_result)
 
 
 @app.route('/get_icon/<icon>', methods=['GET'])
@@ -63,11 +183,71 @@ def get_icon(icon):
 
 @app.route('/show_image', methods=['GET'])
 def show_image():
-    return render_template('img_show.html')
+    global device_list
+    return str(device_list)
+
+
+@app.route('/configuration', methods=['GET', 'POST'])
+def configuration():
+    return render_template('config.html')
+
+
+@app.route('/config_values', methods=['POST', 'GET'])
+def config_values():
+    if request.method == 'GET':
+        config = get_config_data()
+        if config == 0:
+            return json.dumps({"success": 0})
+        else:
+            config["success"] = 1
+            return json.dumps(config)
+
+    if request.method == 'POST':
+        data = request.get_data(as_text=True)
+        json_data = json.loads(data)
+
+        print(data)
+        response = {
+            "success": 1,
+            "message": ""
+        }
+        if check_password(json_data["password"]) == 0:
+            response["success"] = 0
+            response["message"] = 'Password is wrong'
+            return json.dumps(response)
+
+        config = get_config_data()
+
+        config["name"] = json_data["device_name"]
+        config["number_of_devices"] = int(json_data["number_of_devices"])
+        config["device_ip"] = json_data["ip_string"]
+
+        set_config_data(json.dumps(config))
+
+        return json.dumps(response)
 
 
 if __name__ == '__main__':
 
-    print("here")
+    global device_list
+    global total_all_occ
+    total_all_occ = 0
+    global total_all_in, total_all_out
+    total_all_in = 0
+    total_all_out = 0
+    while 1:
+        device_list = get_device_list()
+        if device_list == 0:
+            time.sleep(10)
+        else:
+            config_done = True
+            break
+
+    start_new_thread(print_my_data, ())
+    start_new_thread(maintain_device_list, ())
 
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+# todo refine css and html
+# todo create themes
+
--- test.py
+++ test.py
@@ -1,20 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
-
-# x = requests.get('http://192.168.1.100/cgi-bin/GetCounts.cgi?getReports&2020&04&23&60')
-# # http://<IP>/cgi-bin/GetCounts.cgi?getCounts
-#
-# print(x.text)
-#
-# open("data.xml", "w+").write(x.text)
-# root = ET.fromstring(x.text)
-# # print(root[0][2].attrib)
-# # my_dict=xmltodict.parse(x.text)
-# # print(my_dict)
-# # print(root[1].attrib["In"])
-#
-# for child in root[0]:
-#     print(type(child.attrib["In"]))
+import json
 
 
 def get_aotc(ip):
@@ -30,7 +16,6 @@ def get_aotc(ip):
 
     try:
         response = requests.get(url)
-        # print(response.text)
         if response.status_code != 200:
             data["success"] = 0
             return data
@@ -49,14 +34,9 @@ def get_aotc(ip):
         return data
 
 
-# print(get_aotc('192.168.1.100'))
-
-
 def get_day_count(ip, interval, day):
-
     url = "http://{}/cgi-bin/GetCounts.cgi?getReports&{}&{}&{}&{}".format(ip, day["year"], day["month"], day["day"],
                                                                           interval)
-    # print(url)
     data = {
         "success": 0,
         "in": 0,
@@ -66,7 +46,6 @@ def get_day_count(ip, interval, day):
     }
 
     try:
-        # print("here")
         response = requests.get(url)
         xml_root = ET.fromstring(response.text)
 
@@ -77,65 +56,72 @@ def get_day_count(ip, interval, day):
         data["occupancy"] = data["in"] - data["out"]
         data["success"] = 1
         return data
-
-
     except:
         data["success"] = 0
         return data
 
 
+def get_device_list():
+    device_list = []
+    try:
+        with open("config.json", "r+") as f:
+            config = json.load(f)
+            number_of_devices = config["number_of_devices"]
+
+            for i in range(0, number_of_devices):
+                device_status = {"ip": config["device_ip"][i],
+                                 "final_occ": 0,"final_in": 0, "final_out": 0,
+                                 "now_aotc": 0, "now_aotc_in": 0, "aotc_in_old": 0,
+                                 "now_aotc_out": 0, "aotc_out_old": 0,
+                                 "today_latest_in": 0, "today_latest_in_old": 0,
+                                 "today_latest_out": 0, "today_latest_out_old": 0,
+                                 "today_latest": 0, "active": True}
+
+                device_list.append(device_status)
+        f.close()
+        return device_list
+    except Exception as e:
+        print("error while reading config.json - {}".format(e))
+        f.close()
+        return 0
 
 
+def get_config_data():
 
-def get_data(ip, old_aotc, old_in, old_out):
-
-    data = {
-        "success": 0,
-        "in": 0,
-        "out": 0,
-        "occupancy": 0,
-        "datetime": '',
-        "now_aotc": 0, "now_in": 0, "now_out": 0
-    }
-
-    response = get_aotc(ip)
-    if response["success"] == 0:
-        return data
-    # 04 / 24 / 2020
-    now_aotc = response["occupancy"]
-    now_in = response["in"]
-    now_out = response["out"]
-
-    date = {
-        "day": response["datetime"].split(' ')[0].split('/')[1],
-        "year": response["datetime"].split(' ')[0].split('/')[2],
-        "month": response["datetime"].split(' ')[0].split('/')[0]
-    }
-
-    response2 = get_day_count(ip, '5', date)
-
-    print(response)
-    print(response2)
-
-    present_occupancy = response2["occupancy"] + now_aotc - old_aotc
-    present_in = response2["in"] + now_in - old_in
-    present_out = response2["out"] + now_out - old_out
-
-    data["occupancy"] = present_occupancy
-    data["in"] = present_in
-    data["out"] = present_out
-    data["now_aotc"] = now_aotc
-    data["now_in"] = now_in
-    data["now_out"] = now_out
-
-    return data
+    try:
+        with open("config.json", "r+") as f:
+            config = json.load(f)
+            f.close()
+            return config
+    except Exception as e:
+        print("error while reading config.json - {}".format(e))
+        f.close()
+        return 0
 
 
-#
-# date = {"day": "23", "year": "2020", "month": "04"}
-# result = get_day_count('192.168.1.100', '5', date)
+def set_config_data(data):
 
-# get_data('192.168.1.100', 0, 0, 0)
+    try:
+        with open("config.json", "w") as f:
+            f.write(data)
+            f.close()
+            return 1
+    except Exception as e:
+        print("error while writing to config file - {}".format(e))
+        return 0
 
-# print(result)
-# while
+
+def check_password(string):
+    try:
+        with open("password.txt", "r") as f:
+            password = f.read()
+            if string == password:
+                f.close()
+                return 1
+            else:
+                f.close()
+                return 0
+    except Exception as e:
+        print("error while checking password - {}".format(e))
+        return 0
+
