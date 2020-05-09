19f31bff1e612ed21422de35da3df964e4ca2c7d
nihar <zanwarnihar@gmail.com>
2020-05-09 22:28:26 +0530

    commented restart network as not needed while testing. testing already done on raspi-branch

--- app.py
+++ app.py
@@ -352,9 +352,9 @@ def network_values():
         config["dns_server2"] = json_data["dns_server2"]
 
         set_network_data(json.dumps(config))
-        if restart_network() == 0:
-            response["success"] = 0
-            response["message"] = "Error while restarting network"
+        # if restart_network() == 0:
+        #     response["success"] = 0
+        #     response["message"] = "Error while restarting network"
 
         return json.dumps(response)
 
